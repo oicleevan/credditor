8d874aaa1179ef40dbe77149f8cdfb3ddbecbc7f
oicleevan <oicleevan@protonmail.com>
2021-11-07 12:14:33 -0500

    update stuff

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 import discord
 import sys
 
-token = '' # token requires input at runtime for security reasons
-
-# sets token if presented #
+# token requires input at runtime for security reasons
+token = '' 
 if(len(sys.argv) >= 2):
     token = sys.argv[1]
 else:
@@ -15,15 +14,15 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if message.author == client.user: return
-        
+
         print('Message from {0.author}: {0.content}'.format(message))
 
         if "taiwan" in message.content or "winnie the pooh" in  message.content:
-            await message.channel.send('-100000000 social credit!!!!')
+            await message.reply('-100000000 social credit!!!!')
             return
 
         if message.content.startswith('-social add'):
-            await message.channel.send('+100 social credit')
+            await message.reply('+100 social credit <:tf:783734481672470558>')
             return
 
 client = MyClient()
